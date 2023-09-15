8ce7f25b661646cb09fa2a95f5c50330ead56b1c
Philipp Burckhardt <pburckhardt@outlook.com>
2023-09-15 18:38:14 -0400

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,21 +7,32 @@
 type AccessorFunction<T> = (value: T) => number;
 
 /**
- * Computes the cosine similarity between two arrays.
- *
- * @param x - input array
- * @param y - input array
- * @param accessor - accessor function for accessing array values
- * @returns cosine similarity or null
- */
+* Computes the cosine similarity between two numeric arrays.
+*
+* @param x - numeric array
+* @param y - numeric array
+* @returns cosine similarity or null
+*/
 declare function similarity(
   x: number[],
   y: number[],
 ): number | null;
+
+/**
+* Computes the cosine similarity between two arrays.
+*
+* @param x - input array
+* @param y - input array
+* @param accessor - accessor function for accessing array values
+* @returns cosine similarity or null
+*/
 declare function similarity<T>(
   x: T[],
   y: T[],
   accessor: AccessorFunction<T>
 ): number | null;
 
-export default similarity
+
+// EXPORTS //
+
+export = similarity;
